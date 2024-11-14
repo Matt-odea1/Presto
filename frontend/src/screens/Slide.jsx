96f633f6fae345808502cf86d0a728fbbb0b6057
Matt O'Dea <z5413887@ad.unsw.edu.au>
2024-11-14 11:20:07 +1100

    adding top bar for slide editing styling - no function yet

--- frontend/src/screens/Slide.jsx
+++ frontend/src/screens/Slide.jsx
@@ -2,7 +2,13 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getData } from '../components/Router';
 import Sidebar from '../components/Sidebar';
+import LogoutButton from '../components/Logout';
 import '../styling/Slide.css';
+
+import TextIcon from '../assets/text-icon.svg';
+import ImageIcon from '../assets/img-icon.svg';
+import VideoIcon from '../assets/video-icon.svg';
+import CodeIcon from '../assets/code-icon.svg';
 
 const Slide = () => {
   const { id } = useParams();
@@ -46,29 +52,49 @@ const Slide = () => {
   }
 
   return (
-    <div className="slideContainer">
-      {/* Sidebar on the left */}
-      <Sidebar
-        slides={presentation.slides}
-        activeSlideIndex={activeSlideIndex}
-        setActiveSlide={setActiveSlideIndex}
-        addNewSlide={handleAddSlide}
-      />
-      
-      {/* Slide editing area on the right */}
-      <div className="mainContent">
-        <h2>Editing: {presentation.name}</h2>
-        <p>{presentation.description}</p>
-        <div className="slideEditor">
-          {presentation.slides.length > 0 && (
-            <div className="slideItem">
-              <textarea
-                value={presentation.slides[activeSlideIndex].content}
-                onChange={(e) => handleSlideChange(activeSlideIndex, e.target.value)}
-                placeholder="Enter slide content"
-              />
+    <div className="content-container">
+      {/* Top Bar */}
+      <div className="topBar">
+          <div className="leftSection">
+            <LogoutButton />
+          </div>
+          <div className="centerSection">
+            <img className="topIcon" src={TextIcon} alt="Text Icon" />
+            <img className="topIcon" src={ImageIcon} alt="Image Icon" />
+            <img className="topIcon" src={VideoIcon} alt="Video Icon" />
+            <img className="topIcon" src={CodeIcon} alt="Code Icon" />
+          </div>
+          <div className="rightSection">
+            <div className="stack">
+              <button className="topButton">Edit Thumbnail</button>
+              <button className="topButton">Delete Presentation</button>
             </div>
-          )}
+            <h2 className="slideTitle">{presentation.name}</h2>
+          </div>
+        </div>
+
+      <div className="slideContainer">
+        {/* Sidebar on the left */}
+        <Sidebar
+          slides={presentation.slides}
+          activeSlideIndex={activeSlideIndex}
+          setActiveSlide={setActiveSlideIndex}
+          addNewSlide={handleAddSlide}
+        />
+        
+        {/* Slide editing area on the right */}
+        <div className="mainContent">
+          <div className="slideEditor">
+            {presentation.slides.length > 0 && (
+              <div className="slideItem">
+                <textarea
+                  value={presentation.slides[activeSlideIndex].content}
+                  onChange={(e) => handleSlideChange(activeSlideIndex, e.target.value)}
+                  placeholder="Enter slide content"
+                />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
